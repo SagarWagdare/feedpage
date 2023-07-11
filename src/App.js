c2581d2510c571344624747c0e6d7d20782b1b95
Sagar <120018715+Sagar-appsdeployer@users.noreply.github.com>
2023-07-11 21:54:10 +0530

    Added authentication in Login and Signup page

--- src/App.js
+++ src/App.js
@@ -2,21 +2,31 @@ import "./App.css";
 import Auth from "../src/routes/Auth";
 import UnAuth from "../src/routes/Auth";
 import Header from "./components/Header";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import HomePage from "./components/HomePage";
 import Login from "./AccountManagement/Login";
 import Signup from "./AccountManagement/Signup";
+import { useEffect, useState } from "react";
+import NotFound from "./components/NotFound";
 
 function App() {
+  const [isToken, setIsToken] = useState(false);
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    setIsToken(token);
+  }, []);
   return (
     <>
       <Header />
-      {/* <Auth /> */}
-      {/* <UnAuth /> */}
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        {isToken ? (
+          <Route path="/" element={<HomePage />} />
+        ) : (
+          <Route path="/" element={<NotFound />} />
+        )}
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </>
   );
